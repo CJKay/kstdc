4b996e619b7db5507d590b9e1485bf7718e86a71
CJKay <cjkay93@gmail.com>
2012-09-12 22:45:13 +0100

    Fixed assembly errors

--- include/kstdc/config/asm.h
+++ include/kstdc/config/asm.h
@@ -6,15 +6,15 @@
 	
 #	ifndef __KSTD_NO_INLINEASM__
 #		if (__KSTD_COMPILER__ == gcc) || (__KSTD_COMPILER__ == clang)
-#			define __kstd_gnuasm(text) __asm__(text)
+#			define __kstd_gnuasm(text, ...) __asm__(text : __VA_ARGS__)
 #			define __kstd_noasm(text)
 #		else
 #			warning "No way to implement inline asm on this compiler. Please define __KSTD_NO_INLINEASM__."
-#			define __kstd_gnuasm(text)
+#			define __kstd_gnuasm(text, ...)
 #			define __kstd_noasm(text) { text }
 #		endif
 #	else
-#		define __kstd_gnuasm(text)
+#		define __kstd_gnuasm(text, ...)
 #		define __kstd_noasm(text) { text }
 #	endif
 
