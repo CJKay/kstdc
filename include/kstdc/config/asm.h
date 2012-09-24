7ea7990bba3fd878d17d7b97bae9f4bb99f3f0f4
CJKay <cjkay93@gmail.com>
2012-09-24 19:18:37 +0100

    Volatileld all asm

--- include/kstdc/config/asm.h
+++ include/kstdc/config/asm.h
@@ -4,18 +4,16 @@
 	
 #	include "compiler.h"
 	
-#	ifndef __KSTD_NO_INLINEASM__
+#	if __KSTD_INLINEASM__
 #		if (__KSTD_COMPILER__ == gcc) || (__KSTD_COMPILER__ == clang)
-#			define __kstd_gnuasm(text, ...) __asm__(text : __VA_ARGS__)
+#			define __kstd_gnuasm(text, ...) __asm__ volatile(text : __VA_ARGS__)
 #			define __kstd_noasm(text)
 #		else
-#			warning "No way to implement inline asm on this compiler. Please define __KSTD_NO_INLINEASM__."
-#			define __kstd_gnuasm(text, ...)
-#			define __kstd_noasm(text) { text }
+#			error "No way to implement inline asm on this compiler. Please define __KSTD_NO_INLINEASM__."
 #		endif
 #	else
 #		define __kstd_gnuasm(text, ...)
-#		define __kstd_noasm(text) { text }
+#		define __kstd_noasm(text) do { text } while(0)
 #	endif
 
 #endif
